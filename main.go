96d7dbc21a25c10849c49c5ab518c2c53cac7981
ryurack <73247894+pspd0@users.noreply.github.com>
2022-09-10 06:06:22 +0900

    Add files via upload

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	n := 0
+
+	for n < 1 {
+		id := "rorack"
+
+		req, err := http.Get("https://toss.me/" + id)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer req.Body.Close()
+
+		html, _ := goquery.NewDocumentFromReader(req.Body)
+
+		value := html.Find("span.css-a774xx").Text()
+
+		fmt.Println(value + "\n")
+	}
+}
