b8b9ef5f0e8c8c10e5835194250d1fcb082affcd
ryurack <73247894+pspd0@users.noreply.github.com>
2022-09-10 17:50:28 +0900

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
 	var id string
 	fmt.Print("Enter Your Toss id: ")
@@ -15,17 +21,15 @@ func main() {
 	fmt.Scan(&id)
 
 	fmt.Println("")
-	
-	for i := 0; i < 5; i++ {
-		req, err := http.Get("https://toss.me/" + id)
 
-		if err != nil {
-			fmt.Println(err)
-		}
+	for {
+		req, err := http.Get("https://toss.me/" + id)
+		checkErr(err)
 
 		defer req.Body.Close()
 
-		html, _ := goquery.NewDocumentFromReader(req.Body)
+		html, err := goquery.NewDocumentFromReader(req.Body)
+		checkErr(err)
 
 		value := html.Find("span.css-a774xx").Text()
 
